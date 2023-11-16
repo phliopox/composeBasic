92dab14de98df4b829b43d586885972a540a74cd
phliopox <91457591+phliopox@users.noreply.github.com>
2023-11-16 17:05:54 +0900

    ADD: CustomDialogEx

--- basic_layout/app/src/main/java/com/example/basiclayout/MainActivity.kt
+++ basic_layout/app/src/main/java/com/example/basiclayout/MainActivity.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -15,8 +18,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import com.example.basiclayout.ui.theme.BasiclayoutTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +36,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    DialogEx()
+                    //DialogEx()
+                    CustomDialog()
+
                 }
             }
         }
@@ -67,12 +76,56 @@ fun DialogEx() {
     }
 }
 
+@Composable
+fun CustomDialog() {
+    var openDialog by remember { mutableStateOf(false) }
+    var counter by remember { mutableStateOf(0) }
 
+    Column {
+        Button(onClick = { openDialog = true }) {
+            Text("다이얼로그 열기")
+        }
+        Text("카운터 : $counter")
+    }
+
+    if (openDialog) {
+        Dialog(onDismissRequest = {
+            openDialog = false
+        }) {
+            Surface {
+                Column(Modifier.padding(8.dp)) {
+                    Text("카운터")
+                    Text("버튼을 눌러 카운트 버튼을 조절해보세요")
+                    Row (Modifier.align(Alignment.CenterHorizontally)){
+                        Button(onClick = {
+                            openDialog = false
+                        }) {
+                            Text(text = "취소")
+                        }
+                        Button(onClick = {
+                            counter--
+                            openDialog = false
+                        }) {
+                            Text(text = " -1")
+                        }
+                        Button(onClick = {
+                            counter++
+                            openDialog = false
+                        }) {
+                            Text(text = " +1")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BasiclayoutTheme {
-        DialogEx()
+        //DialogEx()
+        CustomDialog()
     }
 }
