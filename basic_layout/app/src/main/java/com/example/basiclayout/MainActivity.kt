b41dca30948e66e4add795617eeb9678d1c1faee
phliopox <91457591+phliopox@users.noreply.github.com>
2023-11-16 16:57:23 +0900

    ADD: DialogEx

--- basic_layout/app/src/main/java/com/example/basiclayout/MainActivity.kt
+++ basic_layout/app/src/main/java/com/example/basiclayout/MainActivity.kt
@@ -3,10 +3,18 @@ package com.example.basiclayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.basiclayout.ui.theme.BasiclayoutTheme
@@ -30,8 +38,36 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DialogEx() {
+    var openDialog by remember { mutableStateOf(false) }
+    var counter by remember { mutableStateOf(0) }
 
+    Column {
+        Button(onClick = { openDialog = true }) {
+            Text("다이얼로그 열기")
+        }
+        Text("카운터 : $counter")
+    }
+    if (openDialog) {
+        AlertDialog(onDismissRequest = {
+            openDialog = false
+        }, confirmButton = {
+            Button(onClick = {
+                counter++
+                openDialog = false
+            }) {
+                Text(text = "더하기")
+            }
+        }, dismissButton = {
+            Button(onClick = { openDialog = false }) {
+                Text("취소")
+            }
+        },
+            title = { Text(text = "타이틀") },
+            text = { Text("설명문구") })
+    }
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
