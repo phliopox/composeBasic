33120b6b3083fddfbb31a513b1dcadc9c951e6c6
phliopox <91457591+phliopox@users.noreply.github.com>
2023-11-16 16:15:53 +0900

    ADD : canvas practice

--- canvas/app/build.gradle.kts
+++ canvas/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.canvasex"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.canvasex"
@@ -30,11 +30,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
     buildFeatures {
         compose = true
@@ -51,7 +51,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
--- canvas/app/src/main/java/com/example/canvasex/MainActivity.kt
+++ canvas/app/src/main/java/com/example/canvasex/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.canvasex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.canvasex.ui.theme.CanvasexTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    CanvasEx()
                 }
             }
         }
@@ -30,17 +35,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CanvasEx() {
+    Canvas(modifier = Modifier.size(20.dp)) {
+        drawLine(Color.Red, Offset(30f, 10f), Offset(50f, 40f))
+        drawCircle(Color.Yellow, 10f, Offset(15f, 40f))
+        drawRect(Color.Magenta, Offset(30f, 30f), Size(10f, 10f))
+        //        moveTo(2.01f, 21.0f)
+        //        lineTo(23.0f, 12.0f)
+        //        lineTo(2.01f, 3.0f)
+        //        lineTo(2.0f, 10.0f)
+        //        lineToRelative(15.0f, 2.0f)
+        //        lineToRelative(-15.0f, 2.0f)
+        //        close()
+        drawLine(Color.Green, Offset(2.01f,21.0f), Offset(23.0f,12.0f))
+        drawLine(Color.Green, Offset(23.0f,12.0f), Offset(2.01f,3.0f))
+        drawLine(Color.Green, Offset(2.01f,3.0f), Offset(2.0f, 10.0f))
+        drawLine(Color.Green, Offset(2.0f, 10.0f), Offset(17.0f, 12.0f))
+        drawLine(Color.Green, Offset(17.0f, 12.0f), Offset(17.0f, 12.0f))
+        drawLine(Color.Green, Offset(17.0f, 12.0f), Offset(2.0f, 14.0f))
+        drawLine(Color.Green,  Offset(2.0f, 14.0f),  Offset(2.01f,21.0f))
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CanvasexTheme {
-        Greeting("Android")
+        CanvasEx()
     }
 }
