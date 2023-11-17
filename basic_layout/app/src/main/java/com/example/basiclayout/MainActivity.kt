a295500e502630a78c43975b8b4df44716a174b5
phliopox <91457591+phliopox@users.noreply.github.com>
2023-11-17 13:03:32 +0900

    ADD: snackBarEx

--- basic_layout/app/src/main/java/com/example/basiclayout/MainActivity.kt
+++ basic_layout/app/src/main/java/com/example/basiclayout/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.basiclayout
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,13 +15,20 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -30,6 +38,7 @@ import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.example.basiclayout.ui.theme.BasiclayoutTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,7 +52,8 @@ class MainActivity : ComponentActivity() {
                 ) {
                     //DialogEx()
                     //CustomDialog()
-                    DropDownMenuEx()
+                    //DropDownMenuEx()
+                    snackBarEx()
                 }
             }
         }
@@ -146,11 +156,43 @@ fun DropDownMenuEx() {
             ) {
                 DropdownMenuItem(text = { Text("+1") },
                     onClick = {
-                        counter ++
-                        expandDropDownMenu = false })
+                        counter++
+                        expandDropDownMenu = false
+                    })
 
             }
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun snackBarEx() {
+    var counter by remember { mutableStateOf(0) }
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+    //coroutineScope  onclick 이벤트로 사용할 시 컴포즈 내부에서 사용되는 게 아니라서 상관없지만,
+    //컴포즈에서 사용할 경우 (부작용 처리?) LaunchedEffect 를 사용해야한다 .
+
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(snackbarHostState)
+        }) {
+        Column(Modifier.padding(it)) {
+            Button(onClick = {
+                counter++
+                scope.launch {
+                    snackbarHostState.showSnackbar(
+                        message = "카운터는 $counter 입니다.",
+                        actionLabel = "닫기",
+                        duration = SnackbarDuration.Short
+                    )
+                }
+            }) {
+                Text("더하기")
+            }
+        }
+
     }
 }
 
@@ -160,6 +202,7 @@ fun GreetingPreview() {
     BasiclayoutTheme {
         //DialogEx()
         //  CustomDialog()
-        DropDownMenuEx()
+        //DropDownMenuEx()
+        snackBarEx()
     }
 }
