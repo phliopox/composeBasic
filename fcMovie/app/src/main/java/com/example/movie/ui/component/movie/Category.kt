688e4156cc3d6d6d6ed60305c3883c9a68086a33
phliopox <91457591+phliopox@users.noreply.github.com>
2024-02-20 16:51:22 +0900

    CREATE : MovieItem , Category Component

---
+++ fcMovie/app/src/main/java/com/example/movie/ui/component/movie/Category.kt
@@ -0,0 +1,37 @@
+package com.example.movie.ui.component.movie
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun CategoryRow() {
+    Column{
+        CategoryTitle(titleName = "Action")
+        LazyRow(contentPadding = PaddingValues(horizontal = 10.dp)) {
+            //itemIndexed
+            item {
+                MovieItem()
+            }
+        }
+    }
+}
+
+@Composable
+fun CategoryTitle(titleName: String) {
+    Text("Action", modifier = Modifier.padding(10.dp))
+}
+
+
+@Preview
+@Composable
+fun CategoryTowPreview(){
+    CategoryRow()
+}
